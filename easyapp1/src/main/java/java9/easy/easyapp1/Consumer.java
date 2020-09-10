1be7fb46cab3ef69adf9ed9d066ed35db28a96e2
Administrator <Administrator@sumedha-PC>
2020-09-10 17:51:22 +0530

    Uploaded and updated

--- easyapp1/src/main/java/java9/easy/easyapp1/Consumer.java
+++ easyapp1/src/main/java/java9/easy/easyapp1/Consumer.java
@@ -1,25 +1,74 @@
 package java9.easy.easyapp1;
 
 
-import java.text.DecimalFormat;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Consumer {
 	private String id;
 	private String name;
 	private Integer unitConsumed;
 	private String finalPayment;
+	
+	public Consumer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public Consumer(String id, String name, Integer unitConsumed, String finalPayment) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.unitConsumed = unitConsumed;
+		this.finalPayment = finalPayment;
+	}
+
+
 	public Consumer(String id, String name, Integer unitConsumed) {
 		super();
 		this.id = id;
 		this.name = name;
 		this.unitConsumed = unitConsumed;
 	}
+	
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public Integer getUnitConsumed() {
+		return unitConsumed;
+	}
+
+
+	public void setUnitConsumed(Integer unitConsumed) {
+		this.unitConsumed = unitConsumed;
+	}
+
+
+	public String getFinalPayment() {
+		return finalPayment;
+	}
+
+
+	public void setFinalPayment(String finalPayment) {
+		this.finalPayment = finalPayment;
+	}
+
+
 	@Override
 	public String toString() {
 		String output=String.format("%-5s %-20s %-10s %-10s",id,name,unitConsumed,finalPayment);
